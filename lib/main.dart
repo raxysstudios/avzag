d4cf904144cc6cd2fdaf06944b2199b8770d1c6b
alkaitagi <alkaitagi@outlook.com>
2021-05-11 05:31:06 +0300

    Marker layout

--- lib/main.dart
+++ lib/main.dart
@@ -70,8 +70,8 @@ class MapSample extends StatefulWidget {
 }
 
 class MapSampleState extends State<MapSample> {
-  List<bool> selected = [true, false, false];
-  List<String> lects = ["Iron", "Digor", "Pusulmon"];
+  List<bool> selected = [true, false, false, false];
+  List<String> lects = ["Iron", "Digor", "Kabardian", "Temirgoi"];
 
   @override
   Widget build(BuildContext context) {
@@ -79,6 +79,7 @@ class MapSampleState extends State<MapSample> {
       options: MapOptions(
         center: LatLng(51.5, -0.09),
         zoom: 13.0,
+        interactiveFlags: InteractiveFlag.pinchZoom | InteractiveFlag.drag,
       ),
       layers: [
         TileLayerOptions(
@@ -93,26 +94,35 @@ class MapSampleState extends State<MapSample> {
                 point: LatLng(i * 5.0, i * 5.0),
                 builder: (ctx) => Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(Icons.keyboard_arrow_up),
+                    SizedBox(height: 32),
+                    Container(
+                      height: 8,
+                      child: Icon(
+                        Icons.keyboard_arrow_up,
+                        size: 32,
+                      ),
+                    ),
                     TextButton(
                       child: Text(
                         lects[i],
                         style: TextStyle(
                           fontSize: 18,
+                          fontWeight: FontWeight.bold,
                           color: selected[i] ? Colors.blue : Colors.black,
                           shadows: [
                             Shadow(
                               blurRadius: 8.0,
                               color:
-                                  selected[i] ? Colors.white : Colors.black87,
+                                  selected[i] ? Colors.white : Colors.black45,
                             ),
                           ],
                         ),
                       ),
                       onPressed: () =>
                           setState(() => selected[i] = !selected[i]),
-                    ),
+                    )
                   ],
                 ),
               ),
