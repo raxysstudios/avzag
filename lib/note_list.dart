8ced44794b61ff5e72d8eb18500a641c3ab1bb38
alkaitagi <alkaitagi@outlook.com>
2021-05-28 17:06:50 +0300

    Simplify notes text

--- lib/note_list.dart
+++ lib/note_list.dart
@@ -11,18 +11,9 @@ class NoteList extends StatelessWidget {
   Widget build(BuildContext context) {
     return notes == null
         ? Offstage()
-        : Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              for (final n in notes!)
-                Text(
-                  n,
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontStyle: FontStyle.italic,
-                  ),
-                ),
-            ],
+        : Text(
+            notes!.join('\n'),
+            style: TextStyle(fontStyle: FontStyle.italic),
           );
   }
 }
