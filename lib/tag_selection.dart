d13808f138d42f4e85c33ef93e1cf79dcb197207
alkaitagi <alkaitagi@outlook.com>
2021-05-30 10:40:55 +0300

    Add column tag selection

--- lib/tag_selection.dart
+++ lib/tag_selection.dart
@@ -4,11 +4,13 @@ class TagSelection extends StatelessWidget {
   final List<String> tags;
   final ValueSetter<List<String>> setter;
   final List<String>? selected;
+  final bool row;
 
   const TagSelection(
     this.tags,
     this.setter, {
     this.selected,
+    this.row = false,
   });
 
   List<String> get sorted => tags
@@ -29,22 +31,32 @@ class TagSelection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 44,
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: [
-          for (final t in sorted)
-            Padding(
-              padding: const EdgeInsets.all(2),
-              child: InputChip(
-                label: Text(t),
-                selected: selected?.contains(t) ?? false,
-                onPressed: () => toggleTag(t),
-              ),
-            ),
-        ],
+    final chips = sorted.map(
+      (t) => InputChip(
+        label: Text(t),
+        selected: selected?.contains(t) ?? false,
+        onPressed: () => toggleTag(t),
       ),
     );
+
+    return row
+        ? Container(
+            height: 44,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: chips
+                  .map(
+                    (c) => Padding(
+                      padding: const EdgeInsets.all(2),
+                      child: c,
+                    ),
+                  )
+                  .toList(),
+            ),
+          )
+        : Wrap(
+            spacing: 4,
+            children: chips.toList(),
+          );
   }
 }
