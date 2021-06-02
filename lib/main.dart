944f3701294922135dddcdc0da60d30829fb6f35
alkaitagi <alkaitagi@outlook.com>
2021-06-02 20:30:14 +0300

    Improve FB initializer

--- lib/main.dart
+++ lib/main.dart
@@ -8,25 +8,18 @@ void main() {
   runApp(App());
 }
 
-/// We are using a StatefulWidget such that we only create the [Future] once,
-/// no matter how many times our widget rebuild.
-/// If we used a [StatelessWidget], in the event where [App] is rebuilt, that
-/// would re-initialize FlutterFire and make our application re-enter loading state,
-/// which is undesired.
 class App extends StatefulWidget {
-  // Create the initialization Future outside of `build`:
   @override
   _AppState createState() => _AppState();
 }
 
 class _AppState extends State<App> {
-  List<Language> languages = [];
+  final Future<FirebaseApp> initializer = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      // Initialize FlutterFire:
-      future: Firebase.initializeApp(),
+      future: initializer,
       builder: (context, snapshot) {
         return MaterialApp(
           title: 'Avzag',
