3fbd4699f219ecca3e9e9d40ff52ed8e1c4a56d9
alkaitagi <alkaitagi@outlook.com>
2021-06-03 05:00:37 +0300

    Clean up

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:avzag/firebase_builder.dart';
-import 'package:avzag/home/models.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'home/home_page.dart';
