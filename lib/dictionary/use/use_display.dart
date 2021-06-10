2ba72289885682f2fd065cb0cc8713a5ca609ca5
alkaitagi <alkaitagi@outlook.com>
2021-06-10 19:27:10 +0300

    Clean up

--- lib/dictionary/use/use_display.dart
+++ lib/dictionary/use/use_display.dart
@@ -1,7 +1,6 @@
 import 'package:avzag/dictionary/concept/concept_display.dart';
 import 'package:avzag/dictionary/sample/sample_display.dart';
 import 'package:avzag/dictionary/store.dart';
-import 'package:avzag/widgets/note_display.dart';
 import 'package:flutter/material.dart';
 import 'use.dart';
 
