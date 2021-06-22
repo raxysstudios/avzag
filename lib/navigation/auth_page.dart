870bb3c290e91ec5ff6cd9871e4dbf40afb5a9cc
alkaitagi <alkaitagi@outlook.com>
2021-06-23 01:00:52 +0300

    Remove blokcing sign in

--- lib/navigation/auth_page.dart
+++ lib/navigation/auth_page.dart
@@ -15,7 +15,13 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
+  bool signing = false;
+
   Future<void> signIn() async {
+    setState(() {
+      signing = true;
+    });
+
     await FirebaseAuth.instance.signOut();
     await GoogleSignIn().signOut();
     await EditorStore.setLanguage(null);
@@ -29,6 +35,10 @@ class _AuthPageState extends State<AuthPage> {
       idToken: auth.idToken,
     );
     await FirebaseAuth.instance.signInWithCredential(cred);
+
+    setState(() {
+      signing = false;
+    });
   }
 
   @override
@@ -40,98 +50,101 @@ class _AuthPageState extends State<AuthPage> {
         ),
         title: Text('Editors'),
       ),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: ElevatedButton.icon(
-              onPressed: () => showLoadingDialog(
-                context: context,
-                future: signIn().then((_) => setState(() {})),
-              ),
-              icon: Icon(Icons.person_outlined),
-              label: Text(
-                EditorStore.email ?? 'Sign In',
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8, 0, 8, 16),
-            child: RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                style: TextStyle(color: Colors.black87),
-                children: EditorStore.email == null
-                    ? [
-                        TextSpan(
-                          text: 'Sign in with Google to see your options.',
-                        ),
-                      ]
-                    : [
-                        TextSpan(
-                          text:
-                              'With any question regarding the language materials, contact the correspondng editors below.',
-                        ),
-                        if (EditorStore.editing?.isNotEmpty ?? false) ...[
-                          TextSpan(text: '\n\nOr you can edit '),
-                          TextSpan(
-                            text: capitalize(EditorStore.editing!.join(', ')),
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          TextSpan(text: ' youself.'),
-                        ],
-                      ],
-              ),
-            ),
-          ),
-          if (EditorStore.email != null) ...[
-            Divider(height: 0),
-            for (final l in BaseStore.languages)
-              Builder(
-                builder: (context) {
-                  final language = HomeStore.languages[l]!;
-                  final canEdit = EditorStore.canEdit(l);
-                  final editing = l == EditorStore.language;
-                  return ListTile(
-                    leading: Padding(
-                      padding: EdgeInsets.only(top: canEdit ? 8 : 0),
-                      child: LanguageAvatar(language),
+      body: signing
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: ElevatedButton.icon(
+                    onPressed: () => signIn(),
+                    icon: Icon(Icons.person_outlined),
+                    label: Text(
+                      EditorStore.email ?? 'Sign In',
                     ),
-                    title: Text(
-                      capitalize(l),
-                      style: TextStyle(
-                        color: canEdit ? null : Colors.black54,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                      ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(8, 0, 8, 16),
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      style: TextStyle(color: Colors.black87),
+                      children: EditorStore.email == null
+                          ? [
+                              TextSpan(
+                                text:
+                                    'Sign in with Google to see your options.',
+                              ),
+                            ]
+                          : [
+                              TextSpan(
+                                text:
+                                    'With any question regarding the language materials, contact the correspondng editors below.',
+                              ),
+                              if (EditorStore.editing?.isNotEmpty ?? false) ...[
+                                TextSpan(text: '\n\nOr you can edit '),
+                                TextSpan(
+                                  text: capitalize(
+                                      EditorStore.editing!.join(', ')),
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                TextSpan(text: ' youself.'),
+                              ],
+                            ],
                     ),
-                    subtitle: canEdit
-                        ? Text(
-                            editing
-                                ? 'You are editing this language'
-                                : 'You can edit this language',
-                          )
-                        : null,
-                    onTap: canEdit
-                        ? () => EditorStore.setLanguage(
-                              editing ? null : l,
-                            ).then((_) => setState(() {}))
-                        : null,
-                    selected: editing,
-                    trailing: language.contact == null
-                        ? null
-                        : IconButton(
-                            onPressed: () => launch(language.contact!),
-                            icon: Icon(Icons.send_outlined),
-                            color: Colors.black,
-                            tooltip: "Contact editor",
+                  ),
+                ),
+                if (EditorStore.email != null) ...[
+                  Divider(height: 0),
+                  for (final l in BaseStore.languages)
+                    Builder(
+                      builder: (context) {
+                        final language = HomeStore.languages[l]!;
+                        final canEdit = EditorStore.canEdit(l);
+                        final editing = l == EditorStore.language;
+                        return ListTile(
+                          leading: Padding(
+                            padding: EdgeInsets.only(top: canEdit ? 8 : 0),
+                            child: LanguageAvatar(language),
                           ),
-                  );
-                },
-              ),
-          ],
-        ],
-      ),
+                          title: Text(
+                            capitalize(l),
+                            style: TextStyle(
+                              color: canEdit ? null : Colors.black54,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          subtitle: canEdit
+                              ? Text(
+                                  editing
+                                      ? 'You are editing this language'
+                                      : 'You can edit this language',
+                                )
+                              : null,
+                          onTap: canEdit
+                              ? () => EditorStore.setLanguage(
+                                    editing ? null : l,
+                                  ).then((_) => setState(() {}))
+                              : null,
+                          selected: editing,
+                          trailing: language.contact == null
+                              ? null
+                              : IconButton(
+                                  onPressed: () => launch(language.contact!),
+                                  icon: Icon(Icons.send_outlined),
+                                  color: Colors.black,
+                                  tooltip: "Contact editor",
+                                ),
+                        );
+                      },
+                    ),
+                ],
+              ],
+            ),
     );
   }
 }
