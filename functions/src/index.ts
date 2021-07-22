f053f3f050bb521519326fb20b4607164d2b3b04
alkaitagi <alkaitagi@outlook.com>
2021-07-22 17:09:48 +0300

    Lint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,31 +4,31 @@ import algoliasearch from "algoliasearch";
 
 admin.initializeApp();
 const index = algoliasearch(
-        functions.config().algolia.app,
-        functions.config().algolia.key
-    )
-    .initIndex('dictionary');
-    
+    functions.config().algolia.app,
+    functions.config().algolia.key
+)
+    .initIndex("dictionary");
+
 export const addToIndex = functions.firestore
-    .document('languages/{language}/dictionary/{entryID}')
+    .document("languages/{language}/dictionary/{entryID}")
     .onCreate(async (change, context) => {
-        const data = change.data();
-        const baseEntry = {
-            entryID: context.params.entryID,
-            language: context.params.language,
-            forms: data.forms,
-        };
+      const data = change.data();
+      const baseEntry = {
+        entryID: context.params.entryID,
+        language: context.params.language,
+        forms: data.forms,
+      };
 
-        const entries = [];
-        for (const { conceptID } of data.uses) {
-            const concept = await admin.firestore()
-                .doc('meta/dictionary/concepts/' + conceptID)
-                .get()
-                .then(d => d.data());
-            entries.push(Object.assign({ conceptID, ...concept }, baseEntry));
-        }
+      const entries = [];
+      for (const {conceptID} of data.uses) {
+        const concept = await admin.firestore()
+            .doc("meta/dictionary/concepts/" + conceptID)
+            .get()
+            .then((d) => d.data());
+        entries.push(Object.assign({conceptID, ...concept}, baseEntry));
+      }
 
-        return index.saveObjects(entries, { autoGenerateObjectIDIfNotExist: true });
+      return index.saveObjects(entries, {autoGenerateObjectIDIfNotExist: true});
     });
 
 // // Start writing Firebase Functions
