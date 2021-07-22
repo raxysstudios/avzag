6aeebd63ba43cb292839f0fe569d8050708e92bb
alkaitagi <alkaitagi@outlook.com>
2021-07-22 16:54:55 +0300

    Add indexing cloud functions

---
+++ functions/src/index.ts
@@ -0,0 +1,37 @@
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+import * as algoliasearch from "algoliasearch";
+
+admin.initializeApp();
+const index = algoliasearch(...functions.config().algolia)
+    .initIndex('dictionary');
+    
+export const addToIndex = functions.firestore
+    .document('languages/{language}/dictionary/{entryID}')
+    .onCreate(async (change, context) => {
+        const data = change.data();
+        const baseEntry = {
+            entryID: context.params.entryID,
+            language: context.params.language,
+            forms: data.forms,
+        };
+
+        const entries = [];
+        for (const { conceptID } of data.uses) {
+            const concept = await admin.firestore()
+                .doc('meta/dictionary/concepts/' + conceptID)
+                .get()
+                .then(d => d.data());
+            entries.push(Object.assign({ conceptID, ...concept }, baseEntry));
+        }
+
+        return index.addObjects(entries, { autoGenerateObjectIDIfNotExist: true });
+    });
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
