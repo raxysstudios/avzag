f00eadd8c19ea68e2536d7088db026d96bf06fd8
alkaitagi <alkaitagi@outlook.com>
2021-08-02 01:00:37 +0300

    Add view toggle

--- lib/widgets/text_sample_tiles.dart
+++ lib/widgets/text_sample_tiles.dart
@@ -27,23 +27,34 @@ class TextSample {
   }
 }
 
-class TextSampleTiles extends StatelessWidget {
+class TextSampleTiles extends StatefulWidget {
   final List<TextSample>? samples;
   final ValueSetter<List<TextSample>?>? onEdited;
-
   final IconData? icon;
-  final bool scholar;
   final bool row;
 
   const TextSampleTiles({
     this.samples,
     this.onEdited,
     this.icon,
-    this.scholar = true,
     this.row = false,
   });
 
+  @override
+  _TextSampleTilesState createState() => _TextSampleTilesState();
+}
+
+class _TextSampleTilesState extends State<TextSampleTiles> {
+  bool advanced = false;
+
   Widget buildText(TextSample sample) {
+    final extended = this.advanced || widget.onEdited != null;
+    final fields = [
+      if (sample.translation != null) sample.translation,
+      if (extended && sample.ipa != null) '/${sample.ipa}/',
+      if (extended) sample.glossed,
+    ];
+
     final span = TextSpan(
       style: TextStyle(
         color: Colors.black54,
@@ -56,22 +67,20 @@ class TextSampleTiles extends StatelessWidget {
             color: Colors.black87,
           ),
         ),
-        if (scholar)
+        if (fields.length > 0)
           TextSpan(
-            text: [
-              '',
-              sample.translation,
-              if (scholar && sample.ipa != null) '/${sample.ipa}/',
-              if (scholar) sample.glossed,
-            ].where((t) => t != null).join(row ? ' • ' : '\n'),
+            text: ['', ...fields].join(widget.row ? ' • ' : '\n'),
           )
       ],
     );
-    return onEdited == null ? SelectableText.rich(span) : RichText(text: span);
+
+    return widget.onEdited == null
+        ? SelectableText.rich(span)
+        : RichText(text: span);
   }
 
   void showEditor(BuildContext context, int index) {
-    final samples = this.samples ?? [];
+    final samples = widget.samples ?? [];
     if (index == samples.length) samples.add(TextSample(plain: 'sample'));
 
     final result = EditorDialogResult(samples);
@@ -85,7 +94,7 @@ class TextSampleTiles extends StatelessWidget {
     showEditorDialog(
       context,
       result: result,
-      setter: onEdited!,
+      setter: widget.onEdited!,
       title: 'Edit sample',
       content: SingleChildScrollView(
         child: Column(
@@ -106,10 +115,10 @@ class TextSampleTiles extends StatelessWidget {
   ListTile buildTile(BuildContext context, int index) {
     return ListTile(
       leading: Icon(
-        icon,
+        widget.icon,
         color: index == 0 ? null : Colors.transparent,
       ),
-      title: samples?.isEmpty ?? true
+      title: widget.samples?.isEmpty ?? true
           ? Text(
               'Tap to add samples',
               style: TextStyle(
@@ -117,23 +126,23 @@ class TextSampleTiles extends StatelessWidget {
                 color: Colors.black54,
               ),
             )
-          : buildText(samples![index]),
-      onTap: onEdited == null ? null : () => showEditor(context, index),
-      trailing: onEdited == null
+          : buildText(widget.samples![index]),
+      onTap: widget.onEdited == null ? null : () => showEditor(context, index),
+      trailing: widget.onEdited == null
           ? null
           : index == 0
               ? IconButton(
                   onPressed: () => showEditor(
                     context,
-                    samples?.length ?? 0,
+                    widget.samples?.length ?? 0,
                   ),
                   icon: Icon(Icons.add_outlined),
                   tooltip: 'Add sample',
                 )
               : IconButton(
                   onPressed: () {
-                    samples!.removeAt(index);
-                    onEdited!(samples);
+                    widget.samples!.removeAt(index);
+                    widget.onEdited!(widget.samples);
                   },
                   icon: Icon(Icons.remove_outlined),
                   tooltip: 'Remove sample',
@@ -143,13 +152,22 @@ class TextSampleTiles extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (samples == null && onEdited == null) return Offstage();
-    return Column(
+    if (widget.samples == null && widget.onEdited == null) return Offstage();
+    final tiles = Column(
       children: [
         buildTile(context, 0),
-        if (samples != null)
-          for (var i = 1; i < samples!.length; i++) buildTile(context, i),
+        if (widget.samples != null)
+          for (var i = 1; i < widget.samples!.length; i++)
+            buildTile(context, i),
       ],
     );
+    return widget.onEdited == null
+        ? InkWell(
+            onTap: () => setState(() {
+              advanced = !advanced;
+            }),
+            child: tiles,
+          )
+        : tiles;
   }
 }
