e9d881d79184c72fe0a8caa85f5c3226ea195e70
alkaitagi <alkaitagi@outlook.com>
2021-08-01 19:05:49 +0300

    Clean up

--- lib/widgets/editor_dialog.dart
+++ lib/widgets/editor_dialog.dart
@@ -6,59 +6,58 @@ class EditorDialogResult<T> {
   EditorDialogResult(this.value);
 }
 
-Future<EditorDialogResult<T>?> showEditorDialog<T>(
+void showEditorDialog<T>(
   BuildContext context, {
   required EditorDialogResult<T> result,
   required ValueSetter<T?> setter,
   required String title,
   required Widget content,
-}) {
-  return showDialog<EditorDialogResult<T>>(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: Text(title),
-        content: content,
-        actions: [
-          Row(
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                  setter(null);
-                },
-                icon: Icon(
-                  Icons.delete_outline,
-                  color: Colors.red,
+}) =>
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(title),
+          content: content,
+          actions: [
+            Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    setter(null);
+                  },
+                  icon: Icon(
+                    Icons.delete_outline,
+                    color: Colors.red,
+                  ),
+                  highlightColor: Colors.transparent,
+                  splashColor: Colors.red.shade50,
                 ),
-                highlightColor: Colors.transparent,
-                splashColor: Colors.red.shade50,
-              ),
-              Spacer(),
-              TextButton.icon(
-                onPressed: () => Navigator.pop(context),
-                icon: Icon(Icons.close_outlined),
-                label: Text('CANCEL'),
-                style: ButtonStyle(
-                  foregroundColor: MaterialStateProperty.all(Colors.blueGrey),
-                  overlayColor: MaterialStateProperty.all(
-                    Colors.blueGrey.shade50,
+                Spacer(),
+                TextButton.icon(
+                  onPressed: () => Navigator.pop(context),
+                  icon: Icon(Icons.close_outlined),
+                  label: Text('CANCEL'),
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all(Colors.blueGrey),
+                    overlayColor: MaterialStateProperty.all(
+                      Colors.blueGrey.shade50,
+                    ),
                   ),
                 ),
-              ),
-              TextButton.icon(
-                onPressed: () {
-                  Navigator.pop(context);
-                  setter(result.value);
-                },
-                icon: Icon(Icons.done),
-                label: Text('SAVE'),
-              ),
-            ],
-          ),
-        ],
-      );
-    },
-  );
-}
+                TextButton.icon(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    setter(result.value);
+                  },
+                  icon: Icon(Icons.done),
+                  label: Text('SAVE'),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
