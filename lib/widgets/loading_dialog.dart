eeedd2f5b7c8f13be70ea969b568823f34063059
alkaitagi <alkaitagi@outlook.com>
2021-08-02 04:28:21 +0300

    Improve loading dialog

--- lib/widgets/loading_dialog.dart
+++ lib/widgets/loading_dialog.dart
@@ -4,29 +4,32 @@ void showLoadingDialog<T>(
   BuildContext context,
   Future<T> future,
   ValueSetter<T?> callback,
-) =>
-    showDialog<T>(
-      context: context,
-      builder: (context) {
-        future.then(
-          (result) => Navigator.pop(context, result),
-        );
-        return Center(
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(128),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: SizedBox(
-                width: 32,
-                height: 32,
-                child: CircularProgressIndicator(),
-              ),
+) {
+  var cancelled = false;
+  showDialog(
+    context: context,
+    builder: (context) {
+      return Center(
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(128),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: SizedBox(
+              width: 32,
+              height: 32,
+              child: CircularProgressIndicator(),
             ),
           ),
-        );
-      },
-    ).then((result) {
-      if (result != null) callback(result);
-    });
+        ),
+      );
+    },
+  ).then((_) => cancelled = true);
+  future.then((result) {
+    if (!cancelled) {
+      Navigator.pop(context);
+      callback(result);
+    }
+  });
+}
