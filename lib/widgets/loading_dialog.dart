11c2133417eb605133b18d21a8026f2c79ae0c73
alkaitagi <alkaitagi@outlook.com>
2021-08-05 08:29:10 +0300

    Add error snack

--- lib/widgets/loading_dialog.dart
+++ lib/widgets/loading_dialog.dart
@@ -6,31 +6,54 @@ void showLoadingDialog<T>(
   ValueSetter<T>? callback,
 }) {
   var dismissed = false;
+  final dismissible = callback != null;
   showDialog(
     context: context,
-    barrierDismissible: callback != null,
+    barrierDismissible: dismissible,
     builder: (context) {
-      return Center(
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(128),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: SizedBox(
-              width: 32,
-              height: 32,
-              child: CircularProgressIndicator(),
+      return WillPopScope(
+        onWillPop: () => Future.value(dismissible),
+        child: Center(
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(128),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: SizedBox(
+                width: 32,
+                height: 32,
+                child: CircularProgressIndicator(),
+              ),
             ),
           ),
         ),
       );
     },
   ).then((_) => dismissed = true);
+
   future.then((result) {
-    if (!dismissed) {
-      Navigator.pop(context);
-      if (result != null) callback?.call(result);
-    }
+    if (!dismissed) if (result != null) callback?.call(result);
+  }).catchError((error) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Row(
+          children: [
+            Icon(
+              Icons.error_outlined,
+              color: Colors.white,
+            ),
+            SizedBox(width: 8),
+            Text(
+              'Error!',
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+          ],
+        ),
+      ),
+    );
+  }).whenComplete(() {
+    if (!dismissed) Navigator.pop(context);
   });
 }
