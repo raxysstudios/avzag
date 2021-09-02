d92c17af314430d19bc662e9516fb7b6cee78549
alkaitagi <alkaitagi@outlook.com>
2021-09-02 19:17:24 +0300

    Remove unused import

--- lib/navigation/about_card.dart
+++ lib/navigation/about_card.dart
@@ -1,8 +1,6 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class AboutCard extends StatefulWidget {
   @override
