ed536d824934bd3ad84002be06d971e361641cd7
alkaitagi <alkaitagi@outlook.com>
2021-09-02 15:48:23 +0300

    Enable dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,15 @@ class _AppState extends State<App> {
       title: 'Avzag',
       theme: ThemeData(
         primaryColor: Colors.white,
+        accentColor: Colors.grey,
         scaffoldBackgroundColor: Colors.blueGrey.shade50,
+        cardTheme: CardTheme(
+          clipBehavior: Clip.antiAlias,
+        ),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        accentColor: Colors.grey,
         cardTheme: CardTheme(
           clipBehavior: Clip.antiAlias,
         ),
