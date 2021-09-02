ca3b3f6263ecbcc69712a484454ffb81bf06d09c
alkaitagi <alkaitagi@outlook.com>
2021-09-02 18:04:49 +0300

    Theme snackbar

--- lib/widgets/snackbar_manager.dart
+++ lib/widgets/snackbar_manager.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 
 void showSnackbar(BuildContext context, [text = 'Error!']) {
+  final theme = Theme.of(context);
   final messenger = ScaffoldMessenger.of(context);
   messenger.hideCurrentSnackBar();
   messenger.showSnackBar(
     SnackBar(
+      behavior: SnackBarBehavior.floating,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      backgroundColor: theme.colorScheme.surface,
       content: Row(
         children: [
-          Icon(
-            Icons.error_outlined,
-            color: Colors.white,
-          ),
-          SizedBox(width: 8),
+          const Icon(Icons.error_outlined),
+          const SizedBox(width: 8),
           Text(
             text,
+            style: theme.textTheme.bodyText1,
             overflow: TextOverflow.ellipsis,
             maxLines: 1,
           ),
