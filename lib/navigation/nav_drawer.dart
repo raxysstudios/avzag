c6298dbbd8ad288cb29af418d50f7acd2177aa3a
Mæxæmæt <alkaitagi@outlook.com>
2021-09-17 00:46:57 +0300

    Update nav_drawer.dart

--- lib/navigation/nav_drawer.dart
+++ lib/navigation/nav_drawer.dart
@@ -78,7 +78,7 @@ class NavDraver extends StatelessWidget {
                         leading: const Icon(Icons.landscape_outlined),
                         title: const Text('Made with honor in\nNorth Caucasus'),
                         trailing: const Icon(Icons.send_outlined),
-                        onTap: () => launch('https://t.me/studioraxys'),
+                        onTap: () => launch('https://t.me/raxysstudios'),
                       ),
                       FutureBuilder<PackageInfo>(
                         future: PackageInfo.fromPlatform(),
