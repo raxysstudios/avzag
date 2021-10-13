39f7ddc27b9473ef3cb1b4fba85729a7be6c008f
alkaitagi <alkaitagi@outlook.com>
2021-10-13 12:19:06 +0300

    Refactor functions

---
+++ functions/src/dictionary.ts
@@ -0,0 +1,58 @@
+import * as functions from "firebase-functions";
+import algoliasearch from "algoliasearch";
+
+const index = algoliasearch(
+    functions.config().algolia.app,
+    functions.config().algolia.key
+).initIndex("dictionary");
+
+type EntryRecord = {
+    entryID: string;
+    language: string;
+    headword: string;
+    forms: string[];
+    aliases: string[],
+    term: string;
+    definition: string | undefined;
+    tags: string[] | undefined;
+};
+
+export const indexDictionary = functions
+    .region("europe-central2")
+    .firestore.document("languages/{language}/dictionary/{entryID}")
+    .onWrite(async (change, context) => {
+      if (change.before.exists) {
+        await index.deleteBy({
+          filters: "entryID:" + context.params.entryID,
+        });
+      }
+      if (change.after.exists) {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const entry = change.after.data()!;
+        const base = {
+          entryID: context.params.entryID,
+          language: context.params.language,
+          forms: entry.forms.map(({plain}: never) => plain),
+          headword: entry.forms[0].plain,
+        };
+        const records = [];
+
+        for (const use of entry.uses) {
+          const record = Object.assign({term: use.term}, base) as EntryRecord;
+          if (use.tags?.length || entry.tags?.length) {
+            record.tags = (use.tags ?? []).concat(entry.tags ?? []);
+          }
+          if (use.aliases?.length) {
+            record.aliases = use.aliases;
+          }
+          if (use.definition) {
+            record.definition = use.definition;
+          }
+          records.push(record);
+        }
+        await index.saveObjects(
+            records,
+            {autoGenerateObjectIDIfNotExist: true}
+        );
+      }
+    });
