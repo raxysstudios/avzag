df2773d53dfa7340934b2de569c0aa4135b2353a
alkaitagi <alkaitagi@outlook.com>
2021-11-01 00:28:16 +0300

    Rework bottomSheet layout

--- lib/dictionary/entry_page.dart
+++ lib/dictionary/entry_page.dart
@@ -1,18 +1,26 @@
+import 'package:avzag/dictionary/hit_tile.dart';
 import 'package:avzag/dictionary/meaning_tile.dart';
+import 'package:avzag/global_store.dart';
+import 'package:avzag/home/language_flag.dart';
+import 'package:avzag/widgets/page_title.dart';
 import 'package:avzag/widgets/tags_tile.dart';
 import 'package:avzag/widgets/text_sample_tiles.dart';
 import 'package:avzag/widgets/note_tile.dart';
 import 'package:flutter/material.dart';
 import 'entry.dart';
 
-class EntrySliver extends StatelessWidget {
+class EntryPage extends StatelessWidget {
   final Entry entry;
+  final EntryHit hit;
   final ValueSetter<Entry>? onEdited;
+  final ScrollController? scroll;
 
-  const EntrySliver(
+  const EntryPage(
     this.entry, {
-    Key? key,
+    required this.hit,
     this.onEdited,
+    this.scroll,
+    Key? key,
   }) : super(key: key);
 
   bool get editing => onEdited != null;
@@ -23,29 +31,47 @@ class EntrySliver extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverList(
-      delegate: SliverChildListDelegate(
-        [
-          Card(
-            child: Column(
-              children: [
-                TextSampleTiles(
-                  samples: entry.forms,
-                  onEdited:
-                      editing ? (v) => edit(() => entry.forms = v ?? []) : null,
-                  icon: Icons.layers_outlined,
-                  name: 'form',
-                ),
-                TagsTile(
-                  entry.tags,
-                  onEdited: editing ? (v) => edit(() => entry.tags = v) : null,
-                ),
-                NoteTile(
-                  entry.note,
-                  onEdited: editing ? (v) => edit(() => entry.note = v) : null,
-                ),
-              ],
+    return Scaffold(
+      appBar: AppBar(
+        title: PageTitle(
+          title: editing ? 'Entry editor' : hit.headword,
+          subtitle: editing ? GlobalStore.editing : hit.language,
+        ),
+        actions: [
+          Opacity(
+            opacity: 0.4,
+            child: LanguageFlag(
+              GlobalStore
+                  .languages[editing ? GlobalStore.editing : hit.language]!
+                  .flag,
+              offset: const Offset(-40, 4),
+              scale: 9,
             ),
+          ),
+        ],
+      ),
+      body: ListView(
+        controller: scroll,
+        padding: const EdgeInsets.only(bottom: 76),
+        children: [
+          Column(
+            children: [
+              TextSampleTiles(
+                samples: entry.forms,
+                onEdited:
+                    editing ? (v) => edit(() => entry.forms = v ?? []) : null,
+                icon: Icons.layers_outlined,
+                name: 'form',
+              ),
+              TagsTile(
+                entry.tags,
+                onEdited: editing ? (v) => edit(() => entry.tags = v) : null,
+              ),
+              NoteTile(
+                entry.note,
+                onEdited: editing ? (v) => edit(() => entry.note = v) : null,
+              ),
+            ],
           ),
           for (final use in entry.uses)
             Card(
