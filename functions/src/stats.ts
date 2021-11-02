8aefaf88d0a1f1439e2aaaa38089e8ba002af14b
alkaitagi <alkaitagi@outlook.com>
2021-11-02 18:24:06 +0300

    Update stats function

--- functions/src/stats.ts
+++ functions/src/stats.ts
@@ -9,24 +9,21 @@ const dictionary = algoliasearch(
 
 export const collectStats = functions
     .region("europe-central2")
-    .pubsub.schedule("0 5 * * *")
+    .pubsub.schedule("every 6 hours")
     .timeZone("Europe/Moscow")
     .onRun(async () => {
       const db = admin.firestore();
       const langs = await db
           .collection("languages").get()
           .then((d) => d.docs.map((l) => l.id));
-      const editors = await db
-          .doc("meta/editors").get()
-          .then((d) => d.data() as Record<string, string[]>);
 
       for (const lang of langs) {
         await db.doc("languages/" + lang).update({
           stats: {
-            editors: Object
-                .values(editors)
-                .filter((ls) => ls.includes(lang))
-                .length,
+            editors: await db
+                .collection("users")
+                .where("admin", "array-contains", lang)
+                .get().then((s) => s.size),
             dictionary: await dictionary
                 .search("", {
                   facetFilters: ["language:" + lang],
