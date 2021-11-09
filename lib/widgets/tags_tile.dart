407426c4dc79badc03a060c84f9843fe5c55c81d
alkaitagi <alkaitagi@outlook.com>
2021-11-09 05:19:23 +0300

    Revert simple tags

--- lib/widgets/tags_tile.dart
+++ lib/widgets/tags_tile.dart
@@ -1,20 +1,17 @@
-import 'package:avzag/global_store.dart';
 import 'package:avzag/utils/snackbar_manager.dart';
 import 'package:avzag/utils/utils.dart';
 import 'package:avzag/widgets/editor_dialog.dart';
 import 'package:flutter/material.dart';
 
 class TagsTile extends StatelessWidget {
+  final List<String>? tags;
+  final ValueSetter<List<String>?>? onEdited;
+
   const TagsTile(
     this.tags, {
     Key? key,
     this.onEdited,
-    this.algoliaIndex = 'dictionary',
   }) : super(key: key);
-
-  final List<String>? tags;
-  final ValueSetter<List<String>?>? onEdited;
-  final String? algoliaIndex;
 
   @override
   Widget build(BuildContext context) {
@@ -47,120 +44,26 @@ class TagsTile extends StatelessWidget {
   }
 
   void showEditor(BuildContext context) {
-    var result = <String>[...tags ?? []];
-    final index =
-        algoliaIndex == null ? null : GlobalStore.algolia.index(algoliaIndex!);
-    var suggested = <String>[];
-    final controller = TextEditingController(text: tags?.join(' '));
-
-    final form = GlobalKey<FormState>();
-    final theme = Theme.of(context);
-
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Edit tags'),
-          content: SingleChildScrollView(
-            child: Form(
-              key: form,
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              child: StatefulBuilder(
-                builder: (context, setState) {
-                  return Column(
-                    children: [
-                      TextFormField(
-                        controller: controller,
-                        autofocus: true,
-                        onChanged: (value) async {
-                          result = value
-                              .trim()
-                              .split(' ')
-                              .where((e) => e.isNotEmpty)
-                              .toList();
-                          if (index != null) {
-                            final hits = await index.facetQuery(
-                              'tags',
-                              facetQuery: result.isEmpty || value.endsWith(' ')
-                                  ? ''
-                                  : result.last,
-                            );
-                            setState(() {
-                              suggested = hits.map((h) => h.value).toList();
-                            });
-                          }
-                        },
-                        decoration: const InputDecoration(
-                          labelText: 'Space-separated, without #',
-                        ),
-                        validator: emptyValidator,
-                        inputFormatters: [LowerCaseTextFormatter()],
-                      ),
-                      if (index != null) ...[
-                        const Text('Frequent tags'),
-                        ListView(
-                          scrollDirection: Axis.horizontal,
-                          children: [
-                            for (final t in suggested)
-                              InputChip(
-                                label: Text(t),
-                                onPressed: () {
-                                  final base = result.isEmpty ||
-                                          controller.text.endsWith(' ')
-                                      ? result
-                                      : result.sublist(0, result.length - 1);
-                                  controller.text = [...base, t].join(' ');
-                                },
-                              ),
-                          ],
-                        ),
-                      ]
-                    ],
-                  );
-                },
-              ),
-            ),
+    var result = tags?.join(' ');
+    showEditorDialog(
+      context,
+      result: () => result?.split(' ').where((e) => e.isNotEmpty).toList(),
+      callback: onEdited!,
+      title: 'Edit tags',
+      children: [
+        TextFormField(
+          autofocus: true,
+          initialValue: result,
+          onChanged: (value) {
+            result = value.trim();
+          },
+          decoration: const InputDecoration(
+            labelText: 'Space-separated, without #\'s',
           ),
-          actions: [
-            Row(
-              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                    onEdited!(null);
-                  },
-                  icon: const Icon(Icons.delete_outlined),
-                  color: theme.colorScheme.error,
-                  splashColor: theme.colorScheme.error.withOpacity(0.1),
-                ),
-                const Spacer(),
-                TextButton.icon(
-                  onPressed: () => Navigator.pop(context),
-                  icon: const Icon(Icons.close_outlined),
-                  label: const Text('Cancel'),
-                  style: ButtonStyle(
-                    foregroundColor: MaterialStateProperty.all(theme.hintColor),
-                    overlayColor: MaterialStateProperty.all(
-                      theme.hintColor.withOpacity(0.1),
-                    ),
-                  ),
-                ),
-                TextButton.icon(
-                  onPressed: () {
-                    if (form.currentState?.validate() ?? false) {
-                      Navigator.pop(context);
-                      onEdited!(result);
-                    }
-                  },
-                  icon: const Icon(Icons.done_outlined),
-                  label: const Text('Save'),
-                ),
-              ],
-            ),
-          ],
-        );
-      },
+          validator: emptyValidator,
+          inputFormatters: [LowerCaseTextFormatter()],
+        ),
+      ],
     );
   }
 }
