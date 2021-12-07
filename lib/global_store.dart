1b9ef72721c06deade545ccec99dda5531072a21
alkaitagi <alkaitagi@outlook.com>
2021-12-08 00:26:06 +0300

    Update packages & firebase config

--- lib/global_store.dart
+++ lib/global_store.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'firebase_options.dart';
 import 'home/language.dart';
 import 'utils/utils.dart';
 
@@ -80,7 +81,9 @@ class GlobalStore {
 
   static Future<void> _init() async {
     _first = false;
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
     prefs = await SharedPreferences.getInstance();
     algolia = const Algolia.init(
       applicationId: 'NYVVAA43NI',
