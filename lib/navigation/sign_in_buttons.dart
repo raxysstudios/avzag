be60d371a01fe5d23cd1695ac35482d2d6e6fa9a
alkaitagi <alkaitagi@outlook.com>
2022-01-19 21:52:21 +0300

    Add apple auth code

--- lib/navigation/sign_in_buttons.dart
+++ lib/navigation/sign_in_buttons.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io' show Platform;
+import 'dart:math';
+import 'package:crypto/crypto.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart' as apple;
 
 class SignInButtons extends StatefulWidget {
   const SignInButtons({
@@ -75,15 +79,47 @@ class _SignInButtonsState extends State<SignInButtons> {
   }
 
   Future<AuthCredential?> getAppleCredentials() async {
-    // TODO
-    // final user = await GoogleSignIn().signIn();
-    // if (user != null) {
-    //   final auth = await user.authentication;
-    //   return GoogleAuthProvider.credential(
-    //     accessToken: auth.accessToken,
-    //     idToken: auth.idToken,
-    //   );
-    // }
+    String generateNonce([int length = 32]) {
+      const charset =
+          '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+      final random = Random.secure();
+      return List.generate(
+          length, (_) => charset[random.nextInt(charset.length)]).join();
+    }
+
+    String sha256ofString(String input) {
+      final bytes = utf8.encode(input);
+      final digest = sha256.convert(bytes);
+      return digest.toString();
+    }
+
+    if (kIsWeb) {
+      final provider = OAuthProvider('apple.com')
+        ..addScope('email')
+        ..addScope('name');
+      await FirebaseAuth.instance.signInWithPopup(provider);
+    } else {
+      final rawNonce = generateNonce();
+      final nonce = sha256ofString(rawNonce);
+      var redirectURL = 'https://andaxapp.firebaseapp.com/__/auth/handler';
+      var clientID = 'andaxapp';
+      final appleIdCredential =
+          await apple.SignInWithApple.getAppleIDCredential(
+        scopes: [
+          apple.AppleIDAuthorizationScopes.email,
+          apple.AppleIDAuthorizationScopes.fullName,
+        ],
+        webAuthenticationOptions: apple.WebAuthenticationOptions(
+            clientId: clientID, redirectUri: Uri.parse(redirectURL)),
+        nonce: nonce,
+      );
+      // Create an `OAuthCredential` from the credential returned by Apple.
+      return OAuthProvider('apple.com').credential(
+        idToken: appleIdCredential.identityToken,
+        accessToken: appleIdCredential.authorizationCode,
+        rawNonce: rawNonce,
+      );
+    }
   }
 
   @override
@@ -93,7 +129,7 @@ class _SignInButtonsState extends State<SignInButtons> {
         child: Padding(
           padding: EdgeInsets.all(8),
           child: SizedBox.square(
-            dimension: 30,
+            dimension: 24,
             child: CircularProgressIndicator(),
           ),
         ),
