cb5f3d11d6366fcea31f54db014dd87b89907496
alkaitagi <alkaitagi@outlook.com>
2022-07-09 17:22:45 +0300

    Clean up navigation

--- lib/navigation/router.dart
+++ lib/navigation/router.dart
@@ -1,11 +1,11 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:avzag/modules/account/account.dart';
 import 'package:avzag/modules/dictionary/dictionary.dart';
 import 'package:avzag/modules/dictionary/word.dart';
 import 'package:avzag/modules/dictionary/word_editor.dart';
 import 'package:avzag/modules/dictionary/word_loader.dart';
 import 'package:avzag/modules/dictionary/words_diff.dart';
 import 'package:avzag/modules/home/home.dart';
+import 'package:avzag/modules/settings/settings.dart';
 
 import 'root_guard.dart';
 import 'route_builders.dart';
@@ -18,33 +18,10 @@ import 'route_builders.dart';
       page: EmptyRouterScreen,
       name: 'RootRoute',
       guards: [RootGuard],
-    ),
-    AutoRoute<void>(
-      path: '/account',
-      page: AccountScreen,
-    ),
-    AutoRoute<void>(
-      path: '/home',
-      page: HomeScreen,
-    ),
-    AutoRoute<void>(
-      path: '/dictionary',
-      page: EmptyRouterScreen,
-      name: 'DictionaryRootRoute',
       children: [
         AutoRoute<void>(
           path: '',
           page: DictionaryScreen,
-        ),
-        CustomRoute<void>(
-          path: ':id',
-          page: WordLoaderScreen,
-          customRouteBuilder: dialogRouteBuilder,
-        ),
-        CustomRoute<void>(
-          path: ':id',
-          page: WordScreen,
-          customRouteBuilder: sheetRouteBuilder,
         ),
         CustomRoute<void>(
           path: 'editor',
@@ -56,9 +33,27 @@ import 'route_builders.dart';
           page: WordsDiffScreen,
           customRouteBuilder: sheetRouteBuilder,
         ),
+        CustomRoute<void>(
+          path: ':id',
+          page: WordLoaderScreen,
+          customRouteBuilder: dialogRouteBuilder,
+        ),
+        CustomRoute<void>(
+          path: ':id',
+          page: WordScreen,
+          customRouteBuilder: sheetRouteBuilder,
+        ),
       ],
     ),
-    RedirectRoute(path: '*', redirectTo: '/')
+    AutoRoute<void>(
+      path: '/home',
+      page: HomeScreen,
+    ),
+    AutoRoute<void>(
+      path: '/settings',
+      page: SettingsScreen,
+    ),
+    RedirectRoute(path: '*', redirectTo: '/'),
   ],
 )
 class $AppRouter {}
